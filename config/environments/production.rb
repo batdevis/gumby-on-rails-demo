1631a35360c5c9186c21fb2a8fa00cba3167e17d
Devis Battisti <batdevis@gmail.com>
2013-04-18 03:13:04 +0200

    heroku pg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Gumby::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #heroku
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
